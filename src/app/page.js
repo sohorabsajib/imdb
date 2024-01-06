7caf7eb2a8143818cf3ba2209c19583043b20fbb
sohorabsajib <sohorabhossain9521@gmail.com>
2024-01-07 01:43:08 +0600

    error handle

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ const API_KEY = process.env.API_KEY;
 
 export default async function Home({searchParams}) {
   const genre = searchParams.genre || 'fetchTrending';
-  const res =await fetch(`https://api.themoviedb.org/3/${genre === "fetchTopRated" ?  "trending/all/week" :"trending/all/day" }?api_key=${API_KEY}&Language=en-US&page=1`, {next: {revalidate:1000}});
+  const res =await fetch(`https://api.themoviedb.org/3/${genre === "fetchTopRated" ?  "trending/all/week" :"trending/all/day" }?api_key=a${API_KEY}&Language=en-US&page=1`, {next: {revalidate:10000}});
   
   if(!res.ok){
     throw new Error('failed to results');
